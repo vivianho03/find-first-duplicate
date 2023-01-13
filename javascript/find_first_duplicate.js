279bffad52fdbf25bdedff24a4bf1785ee8737d8
Vivian Ho <ho.vivian89@gmail.com>
2023-01-13 11:07:28 -0800

    finished

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,14 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let mySet = new Set()
+
+  for(let i = 0, n = arr.length; i < n ;i++){
+    if(mySet.has(arr[i])){
+      return arr[i]
+    } else {
+      mySet.add(arr[i])
+    }
+  }
+  return -1;
 }
 
 if (require.main === module) {
@@ -17,3 +26,10 @@ module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// What is the first duplicate mean? We want the first occurrence of the second number. 
+// That is the value we return
+// If there are no duplicates => return -1
+// I want to be able to count each number and have it count again if there is a duplicate
+// filter out the duplicates within the array
+// return the first element in the array
